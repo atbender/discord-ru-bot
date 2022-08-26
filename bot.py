29703f0fdddca6c7e2d623b6e6315d170e926afa
atbender <atbender@inf.ufpel.edu.br>
2022-08-25 21:30:27 -0300

    extracted parameter validation

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 # bot.py
 import datetime
 import os
+from typing import Optional
 
 import discord
 from discord.ext import commands
@@ -36,31 +37,34 @@ async def get_ru_tomorrow(ctx):
     await ctx.send(response)
 
 
+def validate_meal(meal: Optional[str]) -> scrap.MealType:
+    if not meal:
+        return scrap.MealType.LUNCH
+    if meal.lower not in ("lunch", "dinner"):
+        return scrap.MealType.LUNCH
+    return scrap.MealType.LUNCH if meal.lower() == "lunch" else scrap.MealType.DINNER
+
+
+def validate_day(day: Optional[str]) -> datetime.date:
+    if not day:
+        return datetime.date.today()
+    if day == "tomorrow":
+        return datetime.date.today() + datetime.timedelta(days=1)
+
+    return datetime.datetime.strptime(day, "%d/%m/%Y")
+
+
 @bot.command(name="ru-meal")
 async def get_ru_meal(ctx, meal=None, day=None):
-    day_formatted: datetime.date
-    meal_type: scrap.MealType
 
-    if not day:
-        day_formatted = datetime.date.today()
-    elif day == "tomorrow":
-        day_formatted = datetime.date.today() + datetime.timedelta(days=1)
-    else:
-        try:
-            day_formatted = datetime.datetime.strptime(day, "%d/%m/%Y")
-        except ValueError:
-            response = "Incorrect data format, should be DD/MM/YYYY"
-            await ctx.send(response)
-            return
+    meal_type: scrap.MealType = validate_meal(meal)
 
-    meal_type = scrap.MealType.LUNCH
-    if not meal:
-        meal_type = scrap.MealType.LUNCH
-    elif meal.lower() not in ("lunch", "dinner"):
-        meal_type = scrap.MealType.LUNCH
-
-    if meal == "dinner":
-        meal_type = scrap.MealType.DINNER
+    try:
+        day_formatted: datetime.date = validate_day(day)
+    except ValueError:
+        response = "Incorrect data format, should be DD/MM/YYYY"
+        await ctx.send(response)
+        return
 
     response = scrap.get_menus(day_formatted, meal_type)
     await ctx.send(response)
